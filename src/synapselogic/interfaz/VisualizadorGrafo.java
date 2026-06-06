a991b6fe71fe25c07dcf3488e4d8000988c6949d
alexramirez <alexramirez@localhost>
2026-06-06 18:05:01 -0400

    Desarrollo Interfaz

--- src/synapselogic/interfaz/VisualizadorGrafo.java
+++ src/synapselogic/interfaz/VisualizadorGrafo.java
@@ -4,10 +4,55 @@
  */
 package synapselogic.interfaz;
 
-/**
- *
- * @author alexramirez
- */
-public class VisualizadorGrafo {
-    
+import java.awt.*;
+import javax.swing.*;
+import synapselogic.dominio.Sinapsis;
+import synapselogic.estructuras.GrafoSinaptico;
+
+public class VisualizadorGrafo extends JPanel {
+
+    private GrafoSinaptico grafo;
+
+    public void mostrarGrafo(GrafoSinaptico grafo) {
+        this.grafo = grafo;
+        repaint();
+    }
+
+    public void resaltarZonasAisladas(java.util.List<String> ids) {
+        repaint();
+    }
+
+    public void resaltarRuta(java.util.List<synapselogic.dominio.Neurona> ruta) {
+        repaint();
+    }
+
+    public void refrescar() {
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (grafo == null) {
+            g.drawString("No hay grafo cargado.", 30, 30);
+            return;
+        }
+
+        int y = 30;
+        g.drawString("Sinapsis cargadas:", 30, y);
+        y += 25;
+
+        for (Sinapsis s : grafo.obtenerTodasLasSinapsis()) {
+            g.drawString(
+                s.getOrigenId() + " -> " + s.getDestinoId() +
+                " | d=" + s.getDistancia() +
+                " | nt=" + s.getIdNeurotransmisor() +
+                " | k=" + s.getCoeficienteEficiencia(),
+                30,
+                y
+            );
+            y += 20;
+        }
+    }
 }
